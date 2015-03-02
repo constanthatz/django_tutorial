b9feb99808b29f8d680c3e3ede7173c195c8ef91
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-01 23:45:56 -0800

    choices.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,17 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):              # __unicode__ on Python 2
+        return unicode(self.question_text)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -11,3 +19,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __unicode__(self):              # __unicode__ on Python 2
+        return unicode(self.choice_text)
